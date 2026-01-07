f34e23285235b2065863926955628b88a90bcb24
Parag Vekariya <154518531+paragvekariya@users.noreply.github.com>
2026-01-07 08:16:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 document.addEventListener("DOMContentLoaded", function() {
   var typed = new Typed('#domains', {
-    strings: [' ', 'Software Engineer.', 'Python Developer.', 'Student..'],
+    strings: [' ', 'Software Engineer.', 'Java Developer.', 'Student..'],
     typeSpeed: 40,
     backspeed: 30,
     loop: true
